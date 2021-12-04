f6431d28d4c2dbe4108d512f99669e36540cb71b
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-12-04 19:18:28 +0530

    2 try

--- 1.sql
+++ 1.sql
@@ -2,3 +2,9 @@ SELECT CustomerName,City FROM Customers;
 
 SELECT * FROM Customers
 WHERE Country='Mexico';
+
+SELECT * FROM Customers
+WHERE Country="GERMANY" and CustomerID = 4
+WHERE NOT Country='Germany';
+
+SELECT * FROM Customers ORDER BY ContactName DESC
