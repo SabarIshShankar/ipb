a1dce4e2293399d1b0d6a27ebbaea290e893490c
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-12-05 11:34:23 +0530

    3 t

--- 1.sql
+++ 1.sql
@@ -8,3 +8,10 @@ WHERE Country="GERMANY" and CustomerID = 4
 WHERE NOT Country='Germany';
 
 SELECT * FROM Customers ORDER BY ContactName DESC
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal','eee', 'skagen', 'stavenger', '4006', 'Norway')
+
+SELECT CustomerName, ContactName, Address
+FROM Customers
+WHERE Address IS NOT NULL
