92f549785d8e9e45906e4e0a63d3623ac2308114
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-01-01 22:03:34 +0530

    try 1

--- 3.sql
+++ 3.sql
@@ -1,3 +1,7 @@
 SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate FROM Orders INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID
 
 SELECT Orders.OrderID, Customers.CustomerName FROM Orders INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID;
+
+SELECT Customers.CustomerName, Orders.OrderID FROM Customers LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+ORDER BY Customers.CustomerName;
+
