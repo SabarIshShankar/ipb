42b89633234294856e2431882df173d59d37308e
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-01-25 22:27:38 +0530

    2

--- sql/w3.sql
+++ sql/w3.sql
@@ -9,3 +9,6 @@ SELECT * FROM Customers WHERE Country="UK" AND (City="London" OR City="Cowes")
 SELECT * FROM Customers ORDER BY Country;
 SELECT * FROM Customers ORDER BY Country Desc;
 INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country) VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway')
+SELECT * FROM Customers WHERE Address is NULL
+UPDATE Customers SET ContactName="AlfredPennyworth", City="Gotham"
+WHERE CustomerID = 1;
