a04482cc3c55b889f775d6776827570ea4adaf26
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-01-25 16:58:24 +0530

    init

---
+++ sql/w3.sql
@@ -0,0 +1,11 @@
+SELECT * FROM Customers
+SELECT DISTINCT Country FROM Customers
+SELECT * FROM Customers WHERE Country="Mexico"
+SELECT * FROM Customers WHERE Country='UK' AND City="London";
+SELECT * FROM Customers WHERE City='London' or City="Cowes"
+SELECT * FROM Customers WHERE NOT Country="Germany" AND NOT Country="UK"
+SELECT * FROM Customers WHERE Country="UK" AND (City="London" OR City="Cowes")
+
+SELECT * FROM Customers ORDER BY Country;
+SELECT * FROM Customers ORDER BY Country Desc;
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country) VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway')
