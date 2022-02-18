a3f56c0b299a597f842106152c8b746691c7b557
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-02-18 20:21:52 +0530

    three

--- sql/w3.sql
+++ sql/w3.sql
@@ -12,3 +12,21 @@ INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Cou
 SELECT * FROM Customers WHERE Address is NULL
 UPDATE Customers SET ContactName="AlfredPennyworth", City="Gotham"
 WHERE CustomerID = 1;
+
+
+SELECT CustomerName, Address + ',' + PostalCode + ',' + City + ',' + Country AS Address FROM Customers
+
+
+--joins
+
+--inner joins
+
+SELECT Orders.OrderID, Orders.OrderDate, Customers.CustomerName --all the fields required
+FROM Orders --the initial table
+INNER JOIN Customers  --where it should be joined
+ON Orders.CustomerID = Customers.CustomerID -- the common relation between the tables
+
+SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName 
+FROM ((Orders 
+INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID)
+INNER JOIN Shippers ON Shippers.ShipperID = Orders.ShipperID)
